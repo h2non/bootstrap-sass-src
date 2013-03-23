42333d74be27939c2375a1e7198cdc91c87f7cf2
Aaron Lademann <aaron.lademann@gmail.com>
2013-03-23 14:00:56 -0700

    github docs

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "_gh_pages/"
+css_dir = "_gh_pages/assets/css"
+sass_dir = "lib"
+fonts_dir = "_gh_pages/assets/fonts"
+images_dir = "_gh_pages/assets/img"
+javascripts_dir = "_gh_pages/assets/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass lib scss && rm -rf sass && mv scss sass
