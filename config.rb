bdc0ba3706aa4c33a2fe30e0684daada7ff1667e
Aaron Lademann <aaron.lademann@gmail.com>
2013-03-23 14:59:56 -0700

    directory cleanup

--- config.rb
+++ config.rb
@@ -3,13 +3,14 @@
 # Set this to the root of your project when deployed:
 http_path = "_gh_pages/"
 css_dir = "_gh_pages/assets/css"
-sass_dir = "lib"
+sass_dir = "sass"
 fonts_dir = "_gh_pages/assets/fonts"
 images_dir = "_gh_pages/assets/img"
 javascripts_dir = "_gh_pages/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+#output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
